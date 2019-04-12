b5bdb3eeb41145986a64b0cb6999bd314a107f85
Sukanya Harris <sukanyaharris1@gmail.com>
2019-04-12 18:33:54 -0400

    Portfolio for Catalyst
    yay HackGT <3

---
+++ Catalyst Portfolio/image_transformation.py
@@ -0,0 +1,26 @@
+from PIL import Image
+
+my_image = Image.open("IMG_6369.JPG")
+image_pixels = my_image.load()
+
+width, height = my_image.size
+for i in range(0, width):
+    for j in range(0, height):
+        current_pixel = image_pixels[i,j]
+        blue_component = image_pixels[i,j][0]
+        green_component = image_pixels[i,j][1]
+        red_component = image_pixels[i,j][2]
+        gray_value = (int)(0.07*blue_component+0.72*green_component+0.21*red_component)
+        image_pixels[i,j] = (gray_value, gray_value, gray_value, 255)
+        if (gray_value < 50):
+            new_color = (0, 31, 59, 255)
+        elif (gray_value < 75):
+            new_color = (215, 30, 1, 255)
+        elif (gray_value < 100):
+            new_color = (107, 153, 160, 255)
+        elif (gray_value < 200):
+            new_color = (182, 198, 186, 255)
+        elif (gray_value < 255):
+            new_color = (250, 237, 192)
+        image_pixels[i,j] = new_color
+my_image.show()
